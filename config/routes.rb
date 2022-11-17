4113efb138fe88103cfd3c5a0e0f183f90d9efe8
Teddy Muller <muller.teddy@icloud.com>
2022-11-17 17:03:49 +0100

    watch list

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # Avoir toute la liste
+  # get '/lists', to: 'lists#index'
+  # # Avoir le detail de chaque liste
+  # get '/lists/:id', to: 'lists#show', as: :list
+  # # Creer une nouvelle liste
+  # get 'lists/new', to: 'lists#new'
+  # post '/lists', to: 'lists#create'
+
+  resources :lists, only:[:index, :show, :new, :create] do
+    resources :bookmarks, only:[:new, :create]
+  end
+  resources :bookmarks, only:[:destroy]
 end
